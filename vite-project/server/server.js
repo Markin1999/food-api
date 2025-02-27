44c06260deca9433957ee6880abb5729062f71cc
Marco Filannino <filanninobusiness@gmail.com>
2025-02-27 10:34:23 +0100

    creato server

---
+++ vite-project/server/server.js
@@ -0,0 +1,18 @@
+import { json } from "express";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(json());
+app.use(cors());
+
+app.get("/", getAll);
+
+app.listen(PORT, () => {
+  console.log(`Server in ascolto alla porta hhtp://localhost:${PORT}`);
+});
